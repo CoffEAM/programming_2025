b477f98e4f9c5723f2621072700a8c35ff8cb707
lenya_official <aziz050907@gmail.com>
2025-09-19 21:08:21 +0200

    EDIT .gitignore EDIT lecture02

--- lecture02/main.cpp
+++ lecture02/main.cpp
@@ -1,6 +1,60 @@
 #include <iostream>
-using namespace std;
+//#include <cmath>
 
 int main(){
-    std::cout << "Hello world";
+    /*
+    int x;
+    int y;
+    std::cin >> x >> y;
+    std::cout << "x = " << x << "\n" << "y = " << y;
+    */
+    
+    // Ариф-кие операции
+    // +, -, *, /, %, ++, --
+    int x = 10;
+    int y = 3;
+    int z = 10 / 3;
+    int mod = 10 % 3;
+
+    // float f = x / y;
+    //float f = (x*1.f) / y;
+    //std::cout << mod << std::endl;
+
+    int k = 10;
+    k = k + 10;
+    k += 10; // -=, *=, /=, %=
+
+    int i = 0;
+    i++; // постфиксный инкремент
+    i--; //декремент
+    ++i; // префиксный инкремент // неявное приведение типа
+
+    int j = 0;
+    std::cout << ++j << std::endl;
+
+    int q = 3.345f; // неявное приведение типа
+    int w = (int)3.345f; // явное приведение типа, c-style cast
+    int e = static_cast<int>(3.345f); // явное приведение типа, C++
+    std::cout << q << std::endl;
+
+    unsigned short d1 = 50000; // 2 ^ 16, 0..65536
+    unsigned short d2 = 60000;
+    unsigned short d3 = d1 + d2;
+    std::cout << d3 << std::endl;
+
+    unsigned short d4 = -1000;
+
+    // CE - compilation error
+    // RE - runtime error
+
+    int d5 = 0;
+    int d6 = 10;
+    //std::cout << d6 / d5 << std::endl;
+    std::cout << d6 / 0 << std::endl;
+
+    const int NMax = 10;
+    // NMax += 1; error const
+    std::cout << NMax << std::endl;
+
+    return 0;
 }
